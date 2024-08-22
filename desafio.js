1b65657e0e6c0de37cda190d7c666b86c5bfe333
Tiago Marques <tiagomarques2283@hotmail.com>
2024-08-22 10:27:57 -0300

    desafio finalizado

--- desafio.js
+++ desafio.js
@@ -1,1 +1,23 @@
-console.log('Olá Mundão');
+let nomeHeroi = 'Tiago';
+let xp = 4521;
+let nivelHeroi = '';
+
+if (xp <= 1000) {
+  nivelHeroi = 'Ferro';
+} else if (xp > 1000 && xp <= 2000) {
+  nivelHeroi = 'Bronze';
+} else if (xp > 2000 && xp <= 5000) {
+  nivelHeroi = 'Prata';
+} else if (xp > 5000 && xp <= 7000) {
+  nivelHeroi = 'Ouro';
+} else if (xp > 7000 && xp <= 8000) {
+  nivelHeroi = 'Platina';
+} else if (xp > 8000 && xp <= 9000) {
+  nivelHeroi = 'Ascendente';
+} else if (xp > 9000 && xp <= 10000) {
+  nivelHeroi = 'Imortal';
+} else if (xp > 10000) {
+  nivelHeroi = 'Radiante';
+}
+
+console.log('O Herói de nome ' + nomeHeroi + ' está no nível de ' + nivelHeroi);
