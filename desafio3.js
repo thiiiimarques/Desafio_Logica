7b8793b668aa164fc544ca840f6bb759ce43ae0c
Tiago Marques <114621406+thiiiimarques@users.noreply.github.com>
2024-08-26 11:21:15 -0300

    Add files via upload
    commit do terceiro desafio

---
+++ desafio3.js
@@ -0,0 +1,28 @@
+class heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque;
+    if (this.tipo === 'mago') {
+      ataque = 'magia';
+    } else if (this.tipo === 'guerreiro') {
+      ataque = 'espada';
+    } else if (this.tipo === 'monge') {
+      ataque = 'artes marciais';
+    } else {
+      ataque = 'shurikens';
+    }
+
+    console.log(`O ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+let heroi1 = new heroi('Tiago', 29, 'ninja');
+heroi1.atacar();
+
+let heroi2 = new heroi('Anita', 26, 'mago');
+heroi2.atacar();
